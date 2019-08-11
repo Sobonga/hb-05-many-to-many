fb1b7ddc55e07d1237090c5fab0b26790171025f
Sobonga Dlabathi <sobongad@qode.healthcare>
2019-08-11 10:45:54 +0200

    many to many

---
+++ src/com/luv2code/hibernate/demo/DeleteMaryStudentDemo.java
@@ -0,0 +1,52 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteMaryStudentDemo {
+
+    public static void main(String[]args){
+
+
+        //Create SessionFactory
+        SessionFactory factory = new Configuration ()
+                .configure ("hibernate.cfg.xml")
+                .addAnnotatedClass (Instructor.class )
+                .addAnnotatedClass (InstructorDetail.class )
+                .addAnnotatedClass (Course.class )
+                .addAnnotatedClass (Review.class )
+                .addAnnotatedClass (Student.class )
+                .buildSessionFactory ();
+
+        //Create Session
+        Session session = factory.getCurrentSession ();
+
+
+        try{
+
+
+            //start a transaction
+            session.beginTransaction ();
+
+            int studentId=2;
+            Student tempStudent = session.get ( Student.class,studentId );
+
+            System.out.println ("Loaded Student "+ tempStudent);
+            System.out.println ("Courses "+tempStudent.getCourses () );
+
+            session.delete ( tempStudent );
+
+            //commit transaction
+            session.getTransaction ().commit ();
+            System.out.println ("Done!!!");
+
+        }
+        finally {
+            //add clean up code
+            session.close ();
+            factory.close ();
+        }
+    }
+}
